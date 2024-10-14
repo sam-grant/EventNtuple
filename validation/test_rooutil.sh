4c906ed98e6be747ebfbaf56117e6852d1facbe6
Andrew Edmonds <aedmonds@york.cuny.edu>
2024-10-14 14:51:35 -0500

    Add RooUtil tests for EventNtuples without MC information

--- validation/test_rooutil.sh
+++ validation/test_rooutil.sh
@@ -1,4 +1,9 @@
-root -l -b -q utils/rooutil/examples/PrintEvents.C++\(\"nts.ntuple.v6.root\"\)
-root -l -b -q utils/rooutil/examples/PlotEntranceMomentum.C++\(\"nts.ntuple.v6.root\"\)
-root -l -b -q utils/rooutil/examples/CreateTree.C++\(\"nts.ntuple.v6.root\"\)
-root -l -b -q utils/rooutil/examples/TimingTest.C++\(\"nts.ntuple.v6.root\"\)
+root -l -b -q utils/rooutil/examples/PrintEvents.C++\(\"nts.ntuple.mock.root\"\)
+root -l -b -q utils/rooutil/examples/PlotEntranceMomentum.C++\(\"nts.ntuple.mock.root\"\)
+root -l -b -q utils/rooutil/examples/CreateTree.C++\(\"nts.ntuple.mock.root\"\)
+root -l -b -q utils/rooutil/examples/TimingTest.C++\(\"nts.ntuple.mock.root\"\)
+
+root -l -b -q utils/rooutil/examples/PrintEventsNoMC.C++\(\"nts.ntuple.mockNoMC.root\"\)
+root -l -b -q utils/rooutil/examples/PlotEntranceMomentum.C++\(\"nts.ntuple.mockNoMC.root\"\)
+root -l -b -q utils/rooutil/examples/CreateTree.C++\(\"nts.ntuple.mockNoMC.root\"\)
+root -l -b -q utils/rooutil/examples/TimingTest.C++\(\"nts.ntuple.mockNoMC.root\"\)
