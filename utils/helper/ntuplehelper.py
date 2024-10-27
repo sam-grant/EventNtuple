6e5807e51ae9e413f09b9105d406b735e83ae3bf
Andrew Edmonds <aedmonds@york.cuny.edu>
2024-10-27 15:23:21 -0500

    Tidy up branch explanation part of the whatis() function

--- utils/helper/ntuplehelper.py
+++ utils/helper/ntuplehelper.py
@@ -117,15 +117,6 @@ class nthelper:
                 tokens=explanation.split(":")
                 print("| " + tokens[0] + " | " + tokens[1] + "| [see " + struct_file + "](../inc/"+struct_file+")")
 
-    # def check_track_type(self, branch):
-    #     retval = ["", ""]
-    #     if "crv" not in branch: # "umm" is matching "crvsummary"
-    #         for key in self.track_types_dict:
-    #             if key in branch: # branch could be "demmc" but key will be "dem"
-    #                 retval = [key, self.track_types_dict[key]]
-    #                 break
-
-    #     return retval
 
     def whatis(self, array):
         if type(array) is not list: # if a single string is passed, put it into an array
@@ -150,43 +141,18 @@ class nthelper:
 
             # Check if this is a track branch
             branch_explanation = self.get_branch_explanation(i_branch)
-            branch_to_search = i_branch
-            track_type, explanation = self.check_track_type(i_branch)
-            branch_output = i_branch + " branch: ";
-            if (explanation != ""):
-                branch_to_search = i_branch.replace(track_type, "trk", 1) # we have keyed all the different track-related branches to "trk" in e.g. branch_struct_dict; also only replace 1 occurance so that klkl branch is handled correctly
-
             leaf_output = "";
             leaf_explanations = {};
             try:
-                struct = self.branch_struct_dict[branch_to_search]
+                struct = self.branch_struct_dict[i_branch]
                 struct_file = struct;
                 if (".hh" not in struct_file):
                     struct_file += ".hh"
-
-
 
 #                print(struct_file)
                 with open(os.environ.get("EVENTNTUPLE_INC")+"/EventNtuple/inc/"+struct_file, 'r') as f:
                     lines = f.readlines()
                     for row in lines:
-                        if (row.find("// "+struct) != -1):
-                            #print(row)
-                            branch_output += row.replace("// "+struct+":", "").replace('\n', ''); # remove the trailing newline as well
-                            if (i_branch in self.single_object_branches):
-                                branch_output += ".\n   - structure: single object"
-                                branch_output += "\n   - object: "+struct
-                            elif (i_branch in self.vector_object_branches):
-                                branch_output += ".\n   - structure: vector of objects"
-                                branch_output += "\n   - object: "+ struct
-                                branch_output += "\n   - example: ["+i_branch+"1, "+i_branch+"2, ..., "+i_branch+"N]"
-                            elif (i_branch in self.vector_vector_object_branches):
-                                branch_output += ".\n   - structure: vector of vector of objects"
-                                branch_output += "\n   - object: "+ struct
-                                # split branch name into "trk" + other thing, and remove the trailing 's' indicating it is plural
-                                split_on="trk"
-                                token=i_branch.split(split_on)[1].rstrip('s')
-                                branch_output += "\n   - example: [ ["+split_on+"1_"+token+"A, "+split_on+"1_"+token+"B, ... ], [ "+split_on+"2_"+token+"A, "+split_on+"2_"+token+"C, ... ], ..., ["+split_on+"N_"+token+"B, "+split_on+"N_"+token+"D, ... ] ]"
                         # Check whether this row is an explanation for a leaf that we are asking for
                         for i_leaf in leaves:
                             if i_leaf == "*":
@@ -200,7 +166,7 @@ class nthelper:
                                     if i_leaf not in leaf_explanations: # we want to only take the first occurance
                                         leaf_explanations[i_leaf] = row
             except KeyError:
-                print(branch_to_search+" is not in branch_struct_dict...\n")
+                print(i_branch+" is not in branch_struct_dict...\n")
 
             # Check that we have all the explanations...
             for i_leaf in leaves:
@@ -210,5 +176,5 @@ class nthelper:
                 leaf_output += i_branch + "." + i_leaf + ": "+leaf_explanations[i_leaf];
 
             # Display the output text
-            print(branch_output)
+            print(branch_explanation)
             print(leaf_output)
