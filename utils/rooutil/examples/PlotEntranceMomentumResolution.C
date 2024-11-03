6f3787dbd91fc601416192306238d14a4390f6b0
Andrew Edmonds <aedmonds@york.cuny.edu>
2024-11-03 17:16:20 -0600

    Add forgotten momentum resolution example

---
+++ utils/rooutil/examples/PlotEntranceMomentumResolution.C
@@ -0,0 +1,44 @@
+//
+// An example of how to plot the momentum of electrons at the tracker entrance
+// This uses cut functions defined in common_cuts.hh
+//
+
+#include "EventNtuple/utils/rooutil/inc/RooUtil.hh"
+#include "EventNtuple/utils/rooutil/inc/common_cuts.hh"
+
+#include "TH1F.h"
+
+void PlotEntranceMomentumResolution(std::string filename) {
+
+  // Create the histogram you want to fill
+  TH1F* hRecoMomRes = new TH1F("hRecoMomRes", "Momentum Resolution at Tracker Entrance", 200,-10,10);
+
+  // Set up RooUtil
+  RooUtil util(filename);
+
+  // Loop through the events
+  for (int i_event = 0; i_event < util.GetNEvents(); ++i_event) {
+    // Get the next event
+    const auto& event = util.GetEvent(i_event);
+
+    // Get the e_minus tracks from the event
+    const auto& e_minus_tracks = event.GetTracks(is_e_minus);
+
+    // Loop through the e_minus tracks
+    for (const auto& track : e_minus_tracks) {
+
+      // Get the track segments at the tracker entrance and has an MC step
+      const auto& trk_ent_segments = track.GetSegments([](const TrackSegment& segment){ return tracker_entrance(segment) && has_mc_step(segment); });
+
+      // Loop through the tracker entrance track segments
+      for (const auto& segment : trk_ent_segments) {
+
+        // Fill the histogram
+        hRecoMomRes->Fill(segment.trkseg->mom.R() - segment.trksegmc->mom.R());
+      }
+    }
+  }
+
+  // Draw the histogram
+  hRecoMomRes->Draw("HIST E");
+}
