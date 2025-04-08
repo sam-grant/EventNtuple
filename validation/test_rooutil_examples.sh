76bf9d8bc00fbb0f231b4eb72895757f6b36a00c
Andrew Edmonds <aedmonds@york.cuny.edu>
2025-04-08 10:38:01 -0500

    Add new example script to validation test

--- validation/test_rooutil_examples.sh
+++ validation/test_rooutil_examples.sh
@@ -1,12 +1,10 @@
-filename="\"../data/nts.mu2e.MDS1f.MDC2020ai_perfect_v1_3.root\""
+filename="\"/pnfs/mu2e/tape/phy-nts/nts/mu2e/CeEndpointOnSpillTriggered/MDC2020aq_best_v1_3_v06_03_00/root/ed/df/nts.mu2e.CeEndpointOnSpillTriggered.MDC2020aq_best_v1_3_v06_03_00.001210_00000000.root\""
 
-root -l -b -q utils/rooutil/examples/PrintEvents.C++\(${filename},true\) # special script that takes an additional argument
-
-scripts=( "CreateNtuple.C" "CreateTrackNtuple.C" "PlotCRVPEs.C" "PlotCRVPEsVsMCEDep.C" "PlotEntranceFitPars.C" "PlotEntranceMomentum.C"
+scripts=( "PrintEvents.C" "CreateNtuple.C" "CreateTrackNtuple.C" "PlotCRVPEs.C" "PlotCRVPEsVsMCEDep.C" "PlotEntranceFitPars.C" "PlotEntranceMomentum.C"
           "PlotEntranceMomentumCRVCut.C" "PlotEntranceMomentumResolution.C" "PlotEntranceMomentumResolution_TrkQualCut.C"
           "PlotMCParentPosZ.C" "PlotMCParticleMom.C" "PlotMuonPosZ.C" "PlotStoppingTargetFoilSegment.C" "PlotTrackNHits_RecoVsTrue.C"
           "PlotTrkCaloHitEnergy.C" "PrintEventsNoMC.C" "TrackCounting.C" "PlotTrackHitTimes.C" "PlotTrackHitTimesMC.C" "PlotStrawMaterials.C"
-          "PlotGenCosmicMom.C" )
+          "PlotGenCosmicMom.C" "PlotCRVTotalPEs.C" )
 
 for script in "${scripts[@]}"
 do
@@ -20,4 +18,4 @@ done
 
 # Check the that reduced ntuple runs in RooUtil
 filename="\"example_ntuple.root\""
-root -l -b -q utils/rooutil/examples/PrintEvents.C++\(${filename},true\) # special script that takes an additional argument
+root -l -b -q utils/rooutil/examples/PrintEvents.C++\(${filename}\)
